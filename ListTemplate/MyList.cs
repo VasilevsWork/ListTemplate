24b8a6196ef9761a64aa765e3e37d41b8199b955
Vasilevs <smaginvd@yandex.ru>
2020-03-14 03:34:06 +0300

    Add project files.

---
+++ ListTemplate/MyList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListTemplate
+{
+    class MyList
+    {
+        // Узел списка
+        private class Node
+        {
+            // Хранимые данные
+            public MyPerson data;
+            // Следующий элемент списка
+            public Node next;
+            // Предыдущий элемент списка
+            public Node prev;
+        }
+
+        Node first = null;
+        Node last = null;
+
+        // Добавление в начало списка
+        public void Append(MyPerson item)
+        {
+
+        }
+
+        // Добавление в конец списка
+        public void Prepend(MyPerson item)
+        {
+
+        }
+
+        // Добавляет элемент на указанную позицию
+        public void Insert(MyPerson item, int postion)
+        {
+
+        }
+
+        // Удаляет элемент по указаннмоу ключу
+        private void Remove(string keyValue)
+        {
+
+        }
+
+        public MyPerson[] ToArray()
+        {
+            return new MyPerson[0];
+        }
+
+        // Ищет элемент списка по указанному ключу (имени)
+        public MyPerson Find(string keyValue)
+        {
+            return new MyPerson();
+        }
+
+        // Поиск узла
+        private Node FindNode(string keyValue)
+        {
+            return null;
+        }
+
+
+    }
+}
---
+++ ListTemplate/MyPerson.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListTemplate
+{
+    class MyPerson
+    {
+        // Поля класса
+        private int age;
+        private bool gender;
+        private string name;
+
+        // Свойства для доступа к полям
+        public int Age { get => age; set => age = value; }
+        public bool Gender { get => gender; set => gender = value; }
+        public string Name { get => name; set => name = value; }
+
+        public MyPerson()
+        {
+            name = "Name";
+            age = 0;
+            Gender = false;
+        }
+
+        public MyPerson(string name, int age, bool gender) 
+        {
+            this.name = name;
+            this.age = age;
+            this.Gender = gender;
+        }
+
+
+    }
+}
